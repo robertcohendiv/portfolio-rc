d610d7c26894aa4c8deebaf470b5e8359451ff58
robertcohen <me@robertdcohen.com>
2022-12-17 21:39:42 -0800

    1217

---
+++ components/About.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+type Props = {};
+
+function About({}: Props) {
+  return (
+    <div className="flex relative flex-col text-center md:text-left md:flex-row max-w-7xl px-10 h-screen justify-evenly mx-auto items-center"> About
+    </div>
+  );
+}
+
+export default About;
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,34 +2,35 @@ import Head from "next/head";
 import { Inter } from "@next/font/google";
 import Header from "../components/header";
 import Hero from "../components/Hero";
+import About from "../components/About";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <div className="bg-[rgb(36,36,36)] text-white h-screen">
+    <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0">
       <Head>
         <title>robert's portfolio</title>
       </Head>
 
-      <Header/>
+      <Header />
 
       {/* Hero  */}
 
-      <section id="hero">
-      <Hero/>
+      <section id="hero" className="snap-start">
+        <Hero />
       </section>
 
-      {/* about */}
-
-      {/* experience */}
+      {/* About */}
+      <section id="about" className="snap-center">
+        <About />
+      </section>start  {/* experience */}
 
       {/* skils */}
 
       {/* project */}
 
       {/* contact me */}
-
     </div>
   );
 }
