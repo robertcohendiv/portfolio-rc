dc43b568aee0ca0b49a5e57f4d77ecaede86263f
robertcohen <me@robertdcohen.com>
2022-12-20 15:31:42 -0800

    201222

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,8 @@ import Header from "../components/header";
 import Hero from "../components/Hero";
 import About from "../components/About";
 import WorkExperience from "../components/WorkExperience";
+import Skills from "../components/Skills";
+import Projects from "../components/Projects";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,38 +13,34 @@ export default function Home() {
   return (
     <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0">
       <Head>
-        <title>robert's portfolio</title>
+      <title>robert's portfolio</title>
       </Head>
 
-      <Header />
+      <Header/>
 
       <section id="hero" className="snap-start">
-        <Hero />
+        <Hero/>
       </section>
 
       <section id="about" className="snap-center">
-        <About />
+        <About/>
       </section>
 
       <section id="workexperience" className="snap-center">
-        <WorkExperience />
+        <WorkExperience/>
       </section>
 
-      <section> 
-        
-        {/* skils */} 
-      
+      <section id='skills' className="snap-start">
+        <Skills/>
       </section>
 
-        <section> 
-        
-       {/* project */}
-      
+      <section id="projects" className="snap-start"> 
+      <Projects/>
       </section>
 
 
       {/* contact me */}
-      d
+      
     </div>
   );
 }
