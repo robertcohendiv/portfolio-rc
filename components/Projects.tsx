bd5bfdafb54c3f555ecf60338c6291ca2d7448c0
robertcohen <me@robertdcohen.com>
2022-12-24 21:50:46 -0800

    241222

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -5,15 +5,14 @@ type Props = {}
 function Projects({}: Props) {
     const projects =[1,2,3,4,5];
   return (
-    <div className="h-screen relative flex overflow-hidden flex-col text-left md:flex-row max-w-full justify-evenly mx-auto item-center z-0 ">
+    <div className="h-screen relative flex overflow-hidden flex-col text-left md:flex-row max-w-full justify-evenly mx-auto item-center z-0">
       <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
         Projects
       </h3>
-      <div className='relative w-full flex overflow-xscroll overflow-y-hidden snap-x snap-mandatory z-20'>
-        {projects.map((project) => (
-                    
-        ))}
-        {/* prodject s */}
+      <div className='flex relative overflow-hidden flex-col text-left md:flex-row max-w-full h-screen justify-evenly mx-auto items-center z-0"'>
+       
+        
+        {/* prodject ss */}
 
       </div>
       <div className='w-full absolute top-[30%] bg-[#F7AB0A]/10 left-0 h-[500px] -skew-y-12'/>
