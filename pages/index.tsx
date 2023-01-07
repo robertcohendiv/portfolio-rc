c74374100a0d7f66375ffaac5f74c864355fd9c7
robertcohen <me@robertdcohen.com>
2023-01-07 08:40:24 -0800

    1322

--- pages/index.tsx
+++ pages/index.tsx
@@ -35,7 +35,7 @@ export default function Home() {
         <Skills />
       </section>
 
-     {/*  <sectsdsdaion id="projects" classNadme="snap-start">
+     {/*  <sectsdsdaionf id="projects" classNadme="snap-start">
         <Projects />
       </section>   d*/}
 
